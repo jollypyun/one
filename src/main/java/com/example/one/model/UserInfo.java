11c15ab40077d9777225691723c0c2cc18aada77
geunhyungpyun <geunhyung@mz.co.kr>
2022-10-11 17:02:01 +0900

    - Add SecurityConfig - Add Authorize - Modify application.yml

---
+++ src/main/java/com/example/one/model/UserInfo.java
@@ -0,0 +1,26 @@
+package com.example.one.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "user_info")
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UserInfo {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "password")
+    private String password;
+}
