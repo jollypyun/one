57b92b7458463abc769bbf9dff1705b4b1e75565
geunhyungpyun <geunhyung@mz.co.kr>
2022-10-11 17:02:01 +0900

    - Add SecurityConfig - Add Authorize - Modify application.yml
    (cherry picked from commit 11c15ab40077d9777225691723c0c2cc18aada77)

--- src/main/java/com/example/one/controller/MainController.java
+++ src/main/java/com/example/one/controller/MainController.java
@@ -1,15 +1,23 @@
 package com.example.one.controller;
 
+import com.example.one.service.OneService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping(value = "/api")
 public class MainController {
-    @GetMapping("/hello")
+    private OneService oneService;
+    @GetMapping(value = "/hello")
     public ResponseEntity<String> hello() {
         return ResponseEntity.ok("hello");
+    }
+
+    @GetMapping(value = "/login")
+    public ResponseEntity<String> login(@RequestParam String id, @RequestParam String password) {
+        return ResponseEntity.ok("login");
     }
 }
---
+++ src/main/java/com/example/one/service/OneService.java
@@ -0,0 +1,8 @@
+package com.example.one.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OneService {
+
+}
