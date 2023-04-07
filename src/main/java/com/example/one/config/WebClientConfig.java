7ce70a89f8bc73d90e1b3883929a2e441948e9af
jollypyun <lucas.stave@gmail.com>
2023-04-07 11:22:28 +0900

    - mariadb로의 전환

---
+++ src/main/java/com/example/one/config/WebClientConfig.java
@@ -0,0 +1,16 @@
+package com.example.one.config;
+
+import lombok.extern.slf4j.XSlf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+@XSlf4j
+public class WebClientConfig {
+    @Bean
+    public WebClient webClient() {
+        return WebClient.builder()
+                .build();
+    }
+}
