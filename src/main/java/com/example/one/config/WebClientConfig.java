9bf5180dc9f3652ba68f996017e220c03f4ca44a
jollypyun <lucas.stave@gmail.com>
2023-04-19 17:43:22 +0900

    - webclient 수정

--- src/main/java/com/example/one/config/WebClientConfig.java
+++ src/main/java/com/example/one/config/WebClientConfig.java
@@ -7,8 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
--- src/main/java/com/example/one/serviceimpl/ExternalServiceImpl.java
+++ src/main/java/com/example/one/serviceimpl/ExternalServiceImpl.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
 import reactor.core.publisher.Mono;
 
 import java.net.URLDecoder;
@@ -14,15 +15,21 @@ import java.nio.charset.StandardCharsets;
 @RequiredArgsConstructor
 @Slf4j
 public class ExternalServiceImpl implements ExternalService {
-    private final WebClient webClient;
+//    private final WebClient webClien+-------t;
 
     @Override
     public void getInfo() {
-        String decode = URLDecoder.decode("2R4dpqjc8wA5hk0mcHWWaxp%2FFtSKWGggntnC4aG%2BcL%2FYTcclK3dqc6etDCmc2autJ0YEuekme85qwh1IlLb3mA%3D%3D", StandardCharsets.UTF_8);
+        DefaultUriBuilderFactory uriBuilderFactory = new DefaultUriBuilderFactory("http://apis.data.go.kr/1262000/CountryKorTradeService2");
+        uriBuilderFactory.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.NONE);
+
+        WebClient webClient = WebClient.builder()
+                .uriBuilderFactory(uriBuilderFactory)
+                .build();
+//        String decode = URLDecoder.decode("2R4dpqjc8wA5hk0mcHWWaxp%2FFtSKWGggntnC4aG%2BcL%2FYTcclK3dqc6etDCmc2autJ0YEuekme85qwh1IlLb3mA%3D%3D", StandardCharsets.UTF_8);
         var block = webClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/getCountryKorTradeList2")
-                        .queryParam("serviceKey", decode)
+                        .queryParam("serviceKey", "2R4dpqjc8wA5hk0mcHWWaxp%2FFtSKWGggntnC4aG%2BcL%2FYTcclK3dqc6etDCmc2autJ0YEuekme85qwh1IlLb3mA%3D%3D")
                         .queryParam("pageNo", 1)
                         .queryParam("numOfRows", 10)
                         .build())
