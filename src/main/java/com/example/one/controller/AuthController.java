86342add946361e0852c6e5936e06e993b68dbe1
jollypyun <lucas.stave@gmail.com>
2023-05-11 18:12:15 +0900

    - mongo 적용

--- src/main/java/com/example/one/controller/AuthController.java
+++ src/main/java/com/example/one/controller/AuthController.java
@@ -9,14 +9,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@RestController
 @Slf4j
 public class AuthController {
     private final AuthService authService;
     @PostMapping("/join")
-    public void join(@RequestBody @Valid JoinMember joinMember) {
+    public void join(@RequestBody JoinMember joinMember) {
         log.info(joinMember.userId());
         authService.insertMember(joinMember);
     }
