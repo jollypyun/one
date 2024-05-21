965cac7b0a8cf753eba8154bac455f2acd6d7e95
geunhyung <lucas.stave@gmail.com>
2024-05-21 19:02:05 +0900

    의존성 설정 및 스웨거 수정

--- src/main/java/config/SwaggerConfiguration.java
+++ src/main/java/config/SwaggerConfiguration.java
@@ -1,25 +1,32 @@
 package config;
 
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfiguration {
     @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
     }
 
-    private Info apiInfo() {
-        return new Info()
-                .title("Atom")
-                .description("양성자, 중성자 개수를 통한 원자 조회 API")
-                .version("1.0.0");
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("원자 관련 사이트")
+                .version("1.0.0")
+                .description("원자 조회 API")
+                .build();
     }
 }
