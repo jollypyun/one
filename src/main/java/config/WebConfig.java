add0446e20f62f66ff77120eed242809e7bc2845
geunhyung <lucas.stave@gmail.com>
2024-06-03 23:02:47 +0900

    cors 설정 - cors 에러 해결 못 함

--- src/main/java/config/WebConfig.java
+++ src/main/java/config/WebConfig.java
@@ -6,11 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    public static final String ALLOWED_METHODS = "GET,POST,HEAD";
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedMethods(ALLOWED_METHODS.split(","));
+        registry.addMapping("*")
+                .allowedOrigins("*")
+                .allowCredentials(false)
+                .allowedHeaders("*")
+                .allowedMethods("*");
     }
 }
