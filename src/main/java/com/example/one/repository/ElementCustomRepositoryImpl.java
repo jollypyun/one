68669ea5d63440feb1b9d9f1c8a0b2db58754042
geunhyung <lucas.stave@gmail.com>
2024-06-21 22:46:58 +0900

    코드 정리

--- src/main/java/com/example/one/repository/ElementCustomRepositoryImpl.java
+++ src/main/java/com/example/one/repository/ElementCustomRepositoryImpl.java
@@ -1,7 +1,6 @@
 package com.example.one.repository;
 
 import com.example.one.model.entity.Element;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,13 +9,11 @@ import static com.example.one.model.entity.QElement.element;
 import static com.querydsl.jpa.JPAExpressions.selectFrom;
 
 @Repository
-@NoArgsConstructor
 public class ElementCustomRepositoryImpl implements ElementCustomRepository{
 
 
     @Override
     public List<Element> findAllByKeyword(String keyword) {
-
         return selectFrom(element)
                 .where(element.fullname.like("%" + keyword + "%"))
                 .fetch();
